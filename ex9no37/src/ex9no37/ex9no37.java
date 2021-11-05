7a7d60b5bec60b600ec7cf1bc4d31371c08fcfd2
admin <admin@host.docker.internal>
2021-11-05 09:57:18 +0900

    ex9no37

---
+++ ex9no37/src/ex9no37/ex9no37.java
@@ -0,0 +1,28 @@
+package ex9no37;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ex9no37 {  //  512p
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new File("data.txt"));
+		int cnt = 0;
+		int totalSum = 0;
+		
+		while (sc.hasNextLine()) {
+			String line = sc.nextLine();
+			Scanner sc2 = new Scanner(line).useDelimiter(", ");
+			int sum = 0;
+			
+			while(sc2.hasNextInt()) {
+				sum += sc2.nextInt();
+			}
+			
+			System.out.println(line + ", sum = " + sum);
+			totalSum += sum;
+			cnt++;
+		}
+		System.out.println("Line : "+ cnt + ", Total : " + totalSum);
+	}
+
+}
